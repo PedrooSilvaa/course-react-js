36dfb722ee40dcc966ebcdbb81044c9ec652d130
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-05-08 13:37:40 -0300

    feat: class on component hierarchy

--- 2-FUNDAMENTOS/fundamentos/src/components/FirstComponents.js
+++ 2-FUNDAMENTOS/fundamentos/src/components/FirstComponents.js
@@ -1,9 +1,12 @@
+import MyComponent from "./MyComponent";
+
 // criando componente como uma arrow funtion
 const FirstComponent = () => {
     return (
         <div>
             <h1>Meu Primeiro componente</h1>
             <p className = "teste">Meu Texto</p>
+            <MyComponent />
         </div>
     );
 };
---
+++ 2-FUNDAMENTOS/fundamentos/src/components/MyComponent.js
@@ -0,0 +1,11 @@
+const MyComponent = () => {
+
+    return (
+        <div>
+            <h1>Sendo reutilizado em vários lugares</h1>
+        </div>
+    )
+
+}
+
+export default MyComponent;
