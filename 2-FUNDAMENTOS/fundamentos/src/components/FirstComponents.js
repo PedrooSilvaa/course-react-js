9ed4f611a5b1f5408e489cfb134ca19cadd85481
PedrooSilvaa <pedrohenriquefdasilva14@gmail.com>
2024-05-07 22:16:44 -0300

    feat: create first component

---
+++ 2-FUNDAMENTOS/fundamentos/src/components/FirstComponents.js
@@ -0,0 +1,11 @@
+// criando componente como uma arrow funtion
+const FirstComponent = () => {
+    return (
+        <div>
+            <h1>Meu Primeiro componente</h1>
+        </div>
+    );
+};
+
+// exportando componente
+export default FirstComponent;
