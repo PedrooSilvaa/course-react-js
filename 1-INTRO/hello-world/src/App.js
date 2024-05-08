01dc9251745bbbe2b1a6733f0b6fa7c0a3246079
PedrooSilvaa <pedrohenriquefdasilva14@gmail.com>
2024-05-07 21:51:25 -0300

    feat: add news tags in app.js

--- 1-INTRO/hello-world/src/App.js
+++ 1-INTRO/hello-world/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World React!</h1>
+      <h2>Emmet funcionou</h2>
+      <p>Meu primeiro paragrafo em react</p>
     </div>
   );
 }
